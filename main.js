0104763307415eb4075660d5ae1919c1b446572e
FabulousStarfish <71825170+FabulousStarfish@users.noreply.github.com>
2021-04-15 14:52:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
+peter_pan="";
+harry_potter="";
+function preload(){
+    peter_pan= loadSound('music.mp3');
+    harry_potter= loadSound('music2.mp3');
+}
 function setup(){
     canvas=createCanvas(800,500);
-    canvas.position(375,187.25);
+    canvas.position(367.5,200);
+    video= createCapture(VIDEO);
+    video.hide();
+}
+function draw(){
+    image(video,0,0,800,500)
 }
